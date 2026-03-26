8f4a899a5a53d57fd4d865a3623ca792bc50caca
Vladislav Pankratov <pankratovvlad1@gmail.com>
2026-03-27 01:15:18 +0300

    Add graphana and clickhouse

---
+++ infra/compose/prod/clickhouse/init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS logs
+(
+    timestamp DateTime64(3) CODEC(Delta, ZSTD),
+    service   LowCardinality(String) CODEC(ZSTD),
+    stream    LowCardinality(String) CODEC(ZSTD),
+    message   String CODEC(ZSTD)
+)
+ENGINE = MergeTree()
+PARTITION BY toDate(timestamp)
+ORDER BY (service, timestamp)
+TTL toDate(timestamp) + INTERVAL 30 DAY
+SETTINGS index_granularity = 8192;
