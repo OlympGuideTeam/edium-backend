a08d5aee0cb6c2f3c799bae600a68ae045abb8ee
Vladislav Pankratov <pankratovvlad1@gmail.com>
2026-04-05 16:57:23 +0300

    Add sms in doorman

--- doorman/internal/transport/dto/otp.go
+++ doorman/internal/transport/dto/otp.go
@@ -4,7 +4,7 @@ import "doorman/internal/domain"
 
 type SendOTPRequest struct {
 	Phone   string         `json:"phone" binding:"required,e164,startswith=+7"`
-	Channel domain.Channel `json:"channel" binding:"required,oneof=tg vk"`
+	Channel domain.Channel `json:"channel" binding:"required,oneof=tg vk sms"`
 }
 
 type VerifyOTPRequest struct {
