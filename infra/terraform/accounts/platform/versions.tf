bc9cd85a957ee8777a0022737c8e0a77272c3b2a
Vladislav Pankratov <pankratovvlad1@gmail.com>
2026-04-08 09:03:22 +0300

    Add sphinx infra

--- infra/terraform/accounts/platform/versions.tf
+++ infra/terraform/accounts/platform/versions.tf
@@ -17,7 +17,6 @@ terraform {
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
-    skip_requesting_account_id  = true
   }
 }
 
