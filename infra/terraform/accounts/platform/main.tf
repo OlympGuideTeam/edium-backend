e58243bf143c59e340979b2dc4c34c9efa65f9f1
Vladislav Pankratov <pankratovvlad1@gmail.com>
2026-04-16 09:47:18 +0300

    create riddler infra

--- infra/terraform/accounts/platform/main.tf
+++ infra/terraform/accounts/platform/main.tf
@@ -99,6 +99,8 @@ module "postgres" {
     "charon_prod"  = { owner = "charon_prod" }
     "caesar_test"  = { owner = "caesar_test" }
     "caesar_prod"  = { owner = "caesar_prod" }
+    "riddler_test" = { owner = "riddler_test" }
+    "riddler_prod" = { owner = "riddler_prod" }
     "sphinx"       = { owner = "sphinx" }
   }
 
@@ -111,6 +113,8 @@ module "postgres" {
     "charon_prod"  = { password = var.charon_pg_password_prod }
     "caesar_test"  = { password = var.caesar_pg_password_test }
     "caesar_prod"  = { password = var.caesar_pg_password_prod }
+    "riddler_test" = { password = var.riddler_pg_password_test }
+    "riddler_prod" = { password = var.riddler_pg_password_prod }
     "sphinx"       = { password = var.sphinx_pg_password }
   }
 }
